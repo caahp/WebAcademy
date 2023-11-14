2e7f298a8f0bccfa30765d18d0030c13ddcd814c
Caroline Pereira <carolline.ps@hotmail.com>
2023-11-13 20:26:26 -0400

    angular 13nov

--- Angular-Project/my-first-project/src/app/app.component.ts
+++ Angular-Project/my-first-project/src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MeuComponenteComponent } from './meu-componente/meu-componente.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MeuComponenteComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'my-first-project';
+  helloWorld = {
+    message: 'Hello World'
+  }
 }
