1b80d67e27a22a043414e6a93b70724337e7dc94
Caroline Pereira <carolline.ps@hotmail.com>
2023-11-24 20:21:29 -0400

    24/11

--- Angular/heroes/src/app/heroes/heroes.component.ts
+++ Angular/heroes/src/app/heroes/heroes.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../Hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Spider Man',
-    power: 88,
-    img: 'https://t2.tudocdn.net/634803?w=1200&h=1200'
-  };
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [] // initalized to an empty array
+  selectedHero?: Hero;
+
+  constructor(private heroService: HeroService) {
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelectHero(hero: Hero) {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void{
+    this.heroes = this.heroService.getHeroes();
+  }
 }
