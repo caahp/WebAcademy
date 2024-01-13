c4d771a34307f593935ff46004bc65deea7f129f
Caroline Pereira <carolline.ps@hotmail.com>
2024-01-12 20:05:35 -0400

    crud alunos aulas

--- cadastro_alunos/routes.js
+++ cadastro_alunos/routes.js
@@ -33,13 +33,13 @@ router.post('/alunos', (req, res) => {
 
 // Alunos - Update Endpoint
 router.put('/alunos/:id', (req,res) => {
-    const id = req.params.id
+    const {id} = req.params
     const {nome, idade} = req.body
     const query = `UPDATE alunos SET nome = '${nome}', idade = ${idade} WHERE id_aluno = ${id}`;
 
     db.query(query, (err, result) => {
         if (err) {
-            res.status(500).json("Erro ao atualizar aluno");
+            res.status(500).json({error: err.message});
         }else{
             res.json(result);
         }
@@ -48,12 +48,12 @@ router.put('/alunos/:id', (req,res) => {
 
 // Alunos - Delete Endpoint
 router.delete('/alunos/:id', (req,res) => {
-    const id = req.params.id
+    const {id} = req.params
     const query = `DELETE FROM alunos WHERE id_aluno = ${id}`;
 
     db.query(query, (err, result) => {
         if (err) {
-            res.status(500).json("Erro ao deletar aluno");
+            res.status(500).json({error: err.message});
         }else{
             res.json(result);
         }
@@ -91,7 +91,7 @@ router.post('/aulas', (req, res) => {
 
 // Aulas - Update Endpoint
 router.put('/aulas/:id', (req, res) => {
-    const id = req.params.id;
+    const {id} = req.params;
     const {nome_aula, descricao} = req.body;
     const query = `UPDATE aulas SET nome_aula = '${nome_aula}', descricao = '${descricao}' WHERE id_aula = ${id}`;
 
@@ -106,16 +106,74 @@ router.put('/aulas/:id', (req, res) => {
 
 // Aulas - Delete Endpoint
 router.delete('/aulas/:id', (req, res) => {
-    const id = req.params.id;
+    const {id} = req.params;
     const query = `DELETE FROM aulas WHERE id_aula = ${id}`;
 
     db.query(query, (err,result) => {
         if (err){
-            res.status(500).json("Erro ao deletar aula");
+            res.status(500).json({error: err.message});
         }else{
             res.json(result);
         }
     });
 });
+
+// AlunosAulas - Read Endpoint
+router.get('/alunosaulas', (req, res) => {
+    const query = "SELECT * FROM alunos_aulas"
+
+    db.query(query, (err, result) => {
+        if (err) {
+            res.status(500).json("Erro ao obter tabela Alunos-Aulas");
+        }
+        else{
+            res.json(result);
+        }
+    });
+});
+
+// AlunosAulas - Create Endpoint
+router.post('/alunosaulas', (req, res) => {
+    const {id_aluno, id_aula}= req.body
+    const query = `INSERT INTO alunos_aulas (id_aluno, id_aula) VALUES (${id_aluno}, ${id_aula})`;
+
+    db.query( query, (err, result) => {
+        if (err) {
+            res.status(500).json({error: err.message});
+        }
+        else{
+            res.json(result);
+        }
+    })
+});
+
+// AlunosAulas - Update Endpoint
+router.put('/alunosaulas/:id', (req, res) => {
+    const {id} = req.params
+    const query = `UPDATE alunos_aulas SET id_aluno = ${id_aluno}, id_aula = ${id_aula} WHERE id_aluno_aula = ${id}`;
+
+    db.query(query, (err, result) => {
+        if (err) {
+            res.status(500).json("Erro ao atualizar tabela Alunos-Aulas");
+        }else{
+            res.json(result);
+        }
+    });
+});
+
+// AlunosAulas - Delete Endpoint
+router.delete('/alunosaulas/:id', (req, res) => {
+    const {id} = req.params
+    const query = `DELETE FROM alunos_aulas WHERE id_aluno_aula = ${id}`;
+
+    db.query(query, (err,result) => {
+        if (err){
+            res.status(500).json({error: err.message});
+        }else{
+            res.json(result);
+        }
+    });
+});
+
 
 module.exports = router;
